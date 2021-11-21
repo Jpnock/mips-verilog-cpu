4cdb2e325cfc9245585bf62bb40ecbf7e75ba986
dharmilshah99 <dharmil.shah@hotmail.com>
2021-11-22 06:22:10 +0800

    Added reset condition. Updated TB.

--- rtl/mips_cpu/cpu_regfile.v
+++ rtl/mips_cpu/cpu_regfile.v
@@ -9,23 +9,32 @@ module regfile (
     input size_t write_data_3_i,
     input logic write_enable_i,
     output size_t read_data_1_o,
-    output size_t read_data_2_o
+    output size_t read_data_2_o,
+    output size_t a
 );
 
-
-  logic [31:0] rf[31:0];
+  size_t regs[4:0];
 
   always_ff @(posedge clk) begin
-    if (write_enable_i == 1) begin
-      rf[addr_3_i] <= write_data_3_i;
+    if (reset_i == 1) begin
+      for (integer idx = 0; idx < 32; idx = idx + 1) begin
+        regs[idx] <= 0;
+      end
+    end else if (write_enable_i == 1) begin
+      regs[addr_3_i] <= write_data_3_i;
     end
   end
 
   always_comb begin
-    if (WEN == 0) begin
-      read_data_1_o = rf[addr_1_i];
-      read_data_2_o = rf[addr_2_i];
+    if (reset_i == 1) begin
+      read_data_1_o = 0;
+      read_data_2_o = 0;
+    end else begin
+      read_data_1_o = regs[addr_1_i];
+      read_data_2_o = regs[addr_2_i];
     end
   end
+
+  assign a = regs[addr_3_i];
 
 endmodule
--- rtl/mips_cpu/regfile_tb.v
+++ rtl/mips_cpu/regfile_tb.v
@@ -1,20 +1,21 @@
+import codes::*;
+
 module reg_file_tb ();
   logic clk;
-  logic reset;
-  logic [5:0] A1;
-  logic [5:0] A2;
-  logic [5:0] A3;
-  logic [31:0] WD3;
-  logic WEN;
-  logic [31:0] RD1;
-  logic [31:0] RD2;
-  logic [31:0] a;
+  logic reset_i;
+  regaddr_t addr_1_i;
+  regaddr_t addr_2_i;
+  regaddr_t addr_3_i;
+  size_t write_data_3_i;
+  logic write_enable_i;
+  size_t read_data_1_o;
+  size_t read_data_2_o;
+  size_t a;
 
   initial begin
 
     $dumpfile("reg_file_tb.vcd");
     $dumpvars(0, reg_file_tb);
-
 
     clk = 0;
     #1;
@@ -28,11 +29,11 @@ module reg_file_tb ();
 
   initial begin
 
-    A1  = 0;
-    A2  = 0;
-    A3  = 1;
-    WD3 = 1;
-    WEN = 1;
+    addr_1_i = 0;
+    addr_2_i = 0;
+    addr_3_i = 1;
+    write_data_3_i = 1;
+    write_enable_i = 1;
 
     #3;
 
@@ -40,9 +41,9 @@ module reg_file_tb ();
 
     #1;
 
-    A3  = 2;
-    WD3 = 2;
-    WEN = 1;
+    addr_3_i = 2;
+    write_data_3_i = 2;
+    write_enable_i = 1;
 
     #3;
 
@@ -50,48 +51,37 @@ module reg_file_tb ();
 
     #1;
 
-    A1  = 1;
-    A2  = 2;
-    A3  = 4;
-    WD3 = 32;
-    WEN = 0;
+    addr_1_i = 1;
+    addr_2_i = 2;
+    addr_3_i = 4;
+    write_data_3_i = 32;
+    write_enable_i = 0;
 
     #3;
 
-    assert (RD1 == 1);
+    assert (read_data_1_o == 1);
 
     #3;
 
-    assert (RD2 == 2);
-
-
-
-
+    assert (read_data_2_o == 2);
 
     #10;
 
-
     $finish;
-
-
-
-
-
-
-
 
   end
 
-  reg_file reg_file (
+  regfile regfile (
       .clk(clk),
-      .A1 (A1),
-      .A2 (A2),
-      .A3 (A3),
-      .WD3(WD3),
-      .WEN(WEN),
-      .RD1(RD1),
-      .RD2(RD2),
-      .a  (a)
+      .reset_i(reset_i),
+      .addr_1_i(addr_1_i),
+      .addr_2_i(addr_2_i),
+      .addr_3_i(addr_3_i),
+      .write_data_3_i(write_data_3_i),
+      .write_enable_i(write_enable_i),
+      .read_data_1_o(read_data_1_o),
+      .read_data_2_o(read_data_2_o),
+      .a(a)
   );
 
 endmodule
