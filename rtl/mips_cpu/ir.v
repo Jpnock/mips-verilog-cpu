1444cd00cec33810d9d9f0ea8f12647647c7c775
Bartek Rzepala <92866485+jeanpi101@users.noreply.github.com>
2021-11-22 13:54:52 +0000

    Update rtl/mips_cpu/ir.v
    Co-authored-by: Jpnock <jpnock@gmail.com>

--- rtl/mips_cpu/ir.v
+++ rtl/mips_cpu/ir.v
@@ -35,34 +35,14 @@ module ir (
     end
 
     always_comb begin
-      if (wen_i && reset_i == 0) begin
-            opcode_o = data[31:26];
-            if (opcode_o == OP_SPECIAL) begin   
-                //r-type 
-                rs_o = data[25:21];
-                rt_o = data[20:16];
-                rd_o = data[15:11];
-                shift_o = data[10:6];
-                funct_o = data[5:0]
-            end else if (opcode_o == OP_J || opcode_o == OP_JAL) begin
-                //j-type
-                target_o = data[25:0];
-            end else begin
-                //i-type
-                rs_o = data[25:21];
-                rt_o = data[20:16];
-                immediate_o = data[15:0];
-            end
-        end else if (reset_i) begin
-            opcode_o = 0;
-            funct_o = 0;
-            shift_o = 0;
-            rs_o = 0;
-            rt_o = 0;
-            rd_o = 0;
-            immediate_o = 0;
-            target_o = 0;
-        end
+      opcode_o = data[31:26];
+      target_o = data[25:0];
+      rs_o = data[25:21];
+      rt_o = data[20:16];
+      rd_o = data[15:11];
+      immediate_o = data[15:0];
+      shift_o = data[10:6];
+      funct_o = data[5:0];
     end
 
 
