5bdb84ffa36d1c0f4f3052f99b1511395890765a
dharmilshah99 <64415802+dharmilshah99@users.noreply.github.com>
2021-11-23 07:53:26 +0800

    Put PC/IR logic on top
    Co-authored-by: Jpnock <jpnock@gmail.com>

--- rtl/mips_cpu/cpu_control.v
+++ rtl/mips_cpu/cpu_control.v
@@ -32,8 +32,6 @@ module control (
         reg_a3_sel_o = 1'bx;
       end
       OP_LW: begin
-        pc_wen_o = (state_i == EXEC2) ? 1 : 0;
-        ir_wen_o = (state_i == EXEC1) ? 1 : 0;
         ram_wen_o = 0;
         ram_rds_o = ((state_i == FETCH) || (state_i == EXEC1)) ? 1 : 0;
         reg_wen_o = (state_i == EXEC2) ? 1 : 0;
