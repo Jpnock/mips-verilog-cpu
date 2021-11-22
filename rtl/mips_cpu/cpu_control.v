e690d83b6fa230ccca76040f84feef660b10dff5
dharmilshah99 <dharmil.shah@hotmail.com>
2021-11-22 18:29:34 +0800

    Basic Control Unit

--- rtl/mips_cpu/cpu_control.v
+++ rtl/mips_cpu/cpu_control.v
@@ -15,4 +15,67 @@ module control (
     output logic reg_a3_sel_o
 );
 
+  always_comb begin
+    case (opcode_i)
+      OP_SW: begin
+        pc_wen_o = (state_i == EXEC2) ? 1 : 0;
+        ir_wen_o = (state_i == EXEC1) ? 1 : 0;
+        ram_wen_o = (state_i == EXEC2) ? 1 : 0;
+        ram_rds_o = (state_i == FETCH) ? 1 : 0;
+        reg_wen_o = 0;
+        src_b_sel_o = (state_i == EXEC2) ? 1 : 0;
+        ram_a_sel_o = (state_i == EXEC2) ? 1 : 0;
+        reg_wd_sel_o = 1'bx;
+        reg_a3_sel_o = 1'bx;
+      end
+      OP_LW: begin
+        pc_wen_o = (state_i == EXEC2) ? 1 : 0;
+        ir_wen_o = (state_i == EXEC1) ? 1 : 0;
+        ram_wen_o = 0;
+        ram_rds_o = ((state_i == FETCH) || (state == EXEC1)) ? 1 : 0;
+        reg_wen_o = (state == EXEC2) ? 1 : 0;
+        src_b_sel_o = (state_i == EXEC1) ? 1 : 0;
+        ram_a_sel_o = (state_i == EXEC1) ? 1 : 0;
+        reg_wd_sel_o = 0;
+        reg_a3_sel_o = 0;
+      end
+      OP_ADDIU: begin
+        pc_wen_o = (state_i == EXEC2) ? 1 : 0;
+        ir_wen_o = (state_i == EXEC1) ? 1 : 0;
+        ram_wen_o = 0;
+        ram_rds_o = (state_i == FETCH) ? 1 : 0;
+        reg_wen_o = (state == EXEC2) ? 1 : 0;
+        src_b_sel_o = (state_i == EXEC2) ? 1 : 0;
+        ram_a_sel_o = 0;
+        reg_wd_sel_o = (state_i == EXEC2) ? 1 : 0;
+        reg_a3_sel_o = 0;
+      end
+      OP_ADDU: begin
+        pc_wen_o = (state_i == EXEC2) ? 1 : 0;
+        ir_wen_o = (state_i == EXEC1) ? 1 : 0;
+        ram_wen_o = 0;
+        ram_rds_o = (state_i == FETCH) ? 1 : 0;
+        reg_wen_o = (state == EXEC2) ? 1 : 0;
+        src_b_sel_o = 0;
+        ram_a_sel_o = 0;
+        reg_wd_sel_o = (state_i == EXEC2) ? 1 : 0;
+        reg_a3_sel_o = (state_i == EXEC2) ? 1 : 0;
+      end
+      OP_JR: begin
+        pc_wen_o = (state_i == EXEC2) ? 1 : 0;
+        ir_wen_o = (state_i == EXEC1) ? 1 : 0;
+        ram_wen_o = 0;
+        ram_rds_o = (state_i == FETCH) ? 1 : 0;
+        reg_wen_o = 0;
+        src_b_sel_o = 0;
+        ram_a_sel_o = 0;
+        reg_wd_sel_o = 0;
+        reg_a3_sel_o = 0;
+      end
+      default: begin
+        $fatal(0, "Instruction undefined.");
+      end
+    endcase
+  end
+
 endmodule
