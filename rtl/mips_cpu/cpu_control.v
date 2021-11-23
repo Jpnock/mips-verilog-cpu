e2377db8da28fc90f7ff977b756397cf922ec6ef
dharmilshah99 <64415802+dharmilshah99@users.noreply.github.com>
2021-11-23 08:22:00 +0800

    Add Control Unit (#18)
    * Goofed when moving files
    
    * Basic Control Unit
    
    * Update gitignore
    
    * Regrouped Logic
    
    * Add control unit
    
    * Add shift signal to IT
    
    * Added shift output
    
    * Put PC/IR logic on top
    
    Co-authored-by: Jpnock <jpnock@gmail.com>
    
    * Put PC/IR logic on top
    
    Co-authored-by: Jpnock <jpnock@gmail.com>
    
    * Put PC/IR logic on top
    
    Co-authored-by: Jpnock <jpnock@gmail.com>
    
    * Put PC/IR logic on top
    
    Co-authored-by: Jpnock <jpnock@gmail.com>
    
    * Put PC/IR logic on top
    
    Co-authored-by: Jpnock <jpnock@gmail.com>
    
    * Put PC/IR logic on top
    
    Co-authored-by: Jpnock <jpnock@gmail.com>
    
    * Makefile: Improve logging on run error
    
    * Makefile: Remove run from all stages
    
    Run should be executed separately as it doesn't make too much sense if the CPU is not hooked up to RAM, etc.
    
    Co-authored-by: Jpnock <jpnock@gmail.com>

--- rtl/mips_cpu/cpu_control.v
+++ rtl/mips_cpu/cpu_control.v
@@ -15,4 +15,67 @@ module control (
     output logic reg_a3_sel_o
 );
 
+  // TODO: Add logic for more instructions.
+
+  always_comb begin
+    pc_wen_o = (state_i == EXEC2) ? 1 : 0;
+    ir_wen_o = (state_i == EXEC1) ? 1 : 0;
+    case (opcode_i)
+      OP_SW: begin
+        ram_wen_o = (state_i == EXEC2) ? 1 : 0;
+        ram_rds_o = (state_i == FETCH) ? 1 : 0;
+        reg_wen_o = 0;
+        src_b_sel_o = (state_i == EXEC2) ? 1 : 0;
+        ram_a_sel_o = (state_i == EXEC2) ? 1 : 0;
+        reg_wd_sel_o = 1'bx;
+        reg_a3_sel_o = 1'bx;
+      end
+      OP_LW: begin
+        ram_wen_o = 0;
+        ram_rds_o = ((state_i == FETCH) || (state_i == EXEC1)) ? 1 : 0;
+        reg_wen_o = (state_i == EXEC2) ? 1 : 0;
+        src_b_sel_o = (state_i == EXEC1) ? 1 : 0;
+        ram_a_sel_o = (state_i == EXEC1) ? 1 : 0;
+        reg_wd_sel_o = 0;
+        reg_a3_sel_o = 0;
+      end
+      OP_ADDIU: begin
+        ram_wen_o = 0;
+        ram_rds_o = (state_i == FETCH) ? 1 : 0;
+        reg_wen_o = (state_i == EXEC2) ? 1 : 0;
+        src_b_sel_o = (state_i == EXEC2) ? 1 : 0;
+        ram_a_sel_o = 0;
+        reg_wd_sel_o = (state_i == EXEC2) ? 1 : 0;
+        reg_a3_sel_o = 0;
+      end
+      OP_SPECIAL: begin
+        case (function_i)
+          FUNC_JR: begin
+            ram_wen_o = 0;
+            ram_rds_o = (state_i == FETCH) ? 1 : 0;
+            reg_wen_o = 0;
+            src_b_sel_o = 0;
+            ram_a_sel_o = 0;
+            reg_wd_sel_o = 0;
+            reg_a3_sel_o = 0;
+          end
+          FUNC_ADDU: begin
+            ram_wen_o = 0;
+            ram_rds_o = (state_i == FETCH) ? 1 : 0;
+            reg_wen_o = (state_i == EXEC2) ? 1 : 0;
+            src_b_sel_o = 0;
+            ram_a_sel_o = 0;
+            reg_wd_sel_o = (state_i == EXEC2) ? 1 : 0;
+            reg_a3_sel_o = (state_i == EXEC2) ? 1 : 0;
+          end
+          default: begin
+            $fatal(0, "Instruction undefined.");
+          end
+        endcase
+      end
+      default: begin
+        $fatal(0, "Instruction undefined.");
+      end
+    endcase
+  end
 endmodule
