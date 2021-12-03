182f4d994e1b557c73e547e0bbaaef70081fd1c6
Louise <ljd20@ic.ac.uk>
2021-12-03 22:23:58 +0000

    Nearly Done

--- test/mips/addu/04_max_neg.asm
+++ test/mips/addu/04_max_neg.asm
@@ -1,9 +1,16 @@
-# Expect: -0x10000
+# Expect: -0xFFFFFFFE
 
 .text
 .globl main
 main:
-    addiu $t1, $t1, -0x8000 
-    addiu $t2, $t2, -0x8000     
-    addu $v0, $t1, $t2
+    addiu $t0, $zero, -0x7FFF
+    sll $t0, $t0, 0x10
+    addu $t1, $t1, -0x7FFF
+    addu $t1, $t1, $t1
+    addiu $t0, $t0, -0x1
+    addu $t0, $t1, $t0 
+    
+    addiu $t2, $t0, 0x0
+     
+    addu $v0, $t0, $t2
     jr $zero
