a0e28d2a08ca1f2dfde25ed4b230b078e8ee71c2
jeanpi101 <bartosz.rzepala20@imperial.ac.uk>
2021-12-03 13:22:12 +0000

    fix beq 3

--- test/mips/beq/01_beq_change.asm
+++ test/mips/beq/01_beq_change.asm
@@ -8,11 +8,13 @@ main:
     beq		$v0, $v1, L1
     addi	$v0, $v0, var1       
     addi	$v0, $v0, var1 
+    jr		$zero				
+    
     
 
 L1: 
     addi	$v0, $v0, var1
-
+    jr		$zero	
 .data
 
 var1: .word 0x00002345
