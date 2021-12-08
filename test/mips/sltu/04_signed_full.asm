51d5dbc040619d333b4b3244b74eece4d9a22541
JoachimSand <joachimliptrotsand@gmail.com>
2021-12-08 17:27:18 +0000

    Added a few more tests for negative numbers. Fixed uncommented line.

--- test/mips/sltu/04_signed_full.asm
+++ test/mips/sltu/04_signed_full.asm
@@ -5,7 +5,7 @@
 main: 
     lw $t1, large
     lw $t0, small
-    #sltu $v0, $t1, $t0 # will break if signed compare is used
+    sltu $v0, $t1, $t0 # will break if signed compare is used
     jr $zero
 
 
