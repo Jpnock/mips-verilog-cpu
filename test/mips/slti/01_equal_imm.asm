a62feeffb3f4b7419d64c9b330b6198f29d4328c
dharmilshah99 <dharmil.shah@hotmail.com>
2021-12-09 04:21:32 +0800

    Ensure Registers are overwritten.

--- test/mips/slti/01_equal_imm.asm
+++ test/mips/slti/01_equal_imm.asm
@@ -3,6 +3,7 @@
 .text
 .globl main
 main:
+    addiu $t0, $t0, 0x9999
     addiu $t1, $t1, 0xFFFF
     slti $v0, $t1, 0xFFFF
     jr $zero
--- test/mips/slti/03_smaller_imm.asm
+++ test/mips/slti/03_smaller_imm.asm
@@ -3,6 +3,7 @@
 .text
 .global main
 main: 
+    addiu $t0, $t0, 0x6999
     addiu $t1, $t1, 0x7659
     slti $v0, $t1, 0xFF88
     jr $zero
