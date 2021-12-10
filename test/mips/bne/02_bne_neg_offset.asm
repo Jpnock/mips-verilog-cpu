6ecb12dce2a463c6943dff482ef630feefcb96fb
Bartek Rzepala <92866485+jeanpi101@users.noreply.github.com>
2021-12-10 21:22:59 +0100

    Update 02_bne_neg_offset.asm

--- test/mips/bne/02_bne_neg_offset.asm
+++ test/mips/bne/02_bne_neg_offset.asm
@@ -3,16 +3,10 @@
 .text
 .globl main
 main:   
-<<<<<<< HEAD
     addiu    $v0, $v0, 0x0559
     li		 $t1, 0x12D2
     beq      $v0, $t1, L2
-=======
-    addiu    $v0, $v0, 0x9559
-    li		 $v1, 0x9559
-    beq      $v0, $v1, L2
->>>>>>> ecf7f64865f601b8830ac6431a6042d340255570
-     
+    
 L1: 
     addiu    $v0, $v0, 0x000A		
     addiu    $v0, $v0, 0x0816
