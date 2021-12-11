dc53dfddbcb52b97eac90cb342ebb73051138cad
Jpnock <jpnock@gmail.com>
2021-12-11 15:04:26 -0800

    test: Fix output of programs that contain repeated hex values

--- test/build_instruction_testbench.sh
+++ test/build_instruction_testbench.sh
@@ -9,6 +9,6 @@ do
     rm "$f.out"
     mips-linux-gnu-objcopy -O binary -j .text "$f.out.reloc" "$f.bin"
     rm "$f.out.reloc"
-    od -An -t x1 "$f.bin" | awk '{$1=$1};1' > "$f.hex"
+    od -v -An -t x1 "$f.bin" | awk '{$1=$1};1' > "$f.hex"
     rm "$f.bin"
 done
